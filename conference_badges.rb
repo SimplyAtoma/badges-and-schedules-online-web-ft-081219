9689e26e31544808dab4d61985ef47492d6a5742
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-18 17:10:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,8 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(array)
-  array.each{|name| badge_maker(name)}
+  badge_array= []
+  array.each{|name| badge_array << badge_maker(name)}
 end
 
 def assign_rooms(array)
